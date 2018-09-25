dbd85fdb15b4c370d193977e9775e68a379bee5f
DESKTOP-S9R8AQ3\bhkdm <bhkdm8571@gmail.com>
2018-09-26 02:42:10 +0900

    초기버전 커밋

---
+++ Dmacro/Main.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Dmacro
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(12, 12);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(91, 21);
+            this.textBox.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(109, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(40, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "시작";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(155, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(40, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "정지";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(12, 40);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 16);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "우편물";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(11, 62);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(76, 16);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "혈맹 출첵";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(11, 84);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(76, 16);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "계정 출첵";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Checked = true;
+            this.checkBox4.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox4.Location = new System.Drawing.Point(11, 106);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(64, 16);
+            this.checkBox4.TabIndex = 6;
+            this.checkBox4.Text = "막피 텔";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Checked = true;
+            this.checkBox5.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox5.Location = new System.Drawing.Point(11, 128);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(92, 16);
+            this.checkBox5.TabIndex = 7;
+            this.checkBox5.Text = "hp 20% 귀환";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(11, 150);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(86, 16);
+            this.checkBox6.TabIndex = 8;
+            this.checkBox6.Text = "checkBox6";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(12, 230);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(320, 180);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 9;
+            this.pictureBox.TabStop = false;
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 12;
+            this.listBox.Location = new System.Drawing.Point(104, 40);
+            this.listBox.Name = "listBox";
+            this.listBox.ScrollAlwaysVisible = true;
+            this.listBox.Size = new System.Drawing.Size(228, 184);
+            this.listBox.TabIndex = 10;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(11, 172);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(86, 16);
+            this.checkBox7.TabIndex = 11;
+            this.checkBox7.Text = "checkBox7";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(11, 194);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(86, 16);
+            this.checkBox8.TabIndex = 12;
+            this.checkBox8.Text = "checkBox8";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(338, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(129, 118);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 417);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.textBox);
+            this.Name = "Main";
+            this.Text = "Dmacro";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Dmacro/Main.cs
@@ -0,0 +1,361 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dmacro
+{
+    public partial class Main : Form
+    {
+        #region DLL Import
+        [System.Runtime.InteropServices.DllImport("User32", EntryPoint = "FindWindow")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [System.Runtime.InteropServices.DllImport("User32", EntryPoint = "SetForegroundWindow")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void keybd_event(uint vk, uint scan, uint flags, uint extraInfo);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        internal static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, int nFlags);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, int wParam, IntPtr lParam);
+
+        [System.Runtime.InteropServices.DllImport("User32.dll")]
+        public static extern IntPtr FindWindowEx(IntPtr Parent, IntPtr Child, string lpszClass, string lpszWindows);
+
+        #endregion
+
+        System.Windows.Forms.Timer timer;
+        string AppPlayerName = "";
+        public const int WM_LBUTTONDOWN = 0x201;
+        public const int WM_LBUTTONUP = 0x202;
+        int R = 0,G = 0,B=0;
+        int pkCnt = 0;
+        public Main()
+        {
+            InitializeComponent();
+
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000;
+            timer.Tick += new EventHandler(run);
+        }
+        private void run(object sender, EventArgs e)
+        {
+            Bitmap bmp = PrintScreenAndCheck();
+            if (bmp == null)
+                return;
+
+            try
+            {
+                // 사각 잘라내서 보여주기, PK 감지
+                Rectangle rects = new Rectangle(868, 388, 70, 90);
+                var cloned = new Bitmap(bmp).Clone(rects, bmp.PixelFormat);
+                pictureBox1.Image = cloned;
+
+                //  PK 감지 텔
+                if (checkBox4.Checked)
+                {
+                    if (searchIMG(cloned, Resource.pk))
+                    {
+                        if (pkCnt==0)
+                        {
+                            playTel();
+                        }
+                        if (pkCnt == 3)
+                            pkCnt = 0;
+                        pkCnt++;
+                    }
+                }
+                // hp20 귀환
+                if (checkBox5.Checked)
+                {
+                    if (R < 175 && G > 10 && B > 10)
+                    {
+                        playGoHome();
+                        checkBox5.Checked = false;
+                    }
+                }
+                // 우편물 받기
+                if (checkBox1.Checked)
+                {
+                    if (DateTime.Now.Minute == 0 && DateTime.Now.Second == 0)
+                    {
+                        getPost();
+                    }
+                }
+                // 혈맹 출첵
+                if (checkBox2.Checked)
+                {
+                    if (DateTime.Now.Hour % 12 == 0 && DateTime.Now.Minute == 2 && DateTime.Now.Second == 0)
+                    {
+                        getGCheck();
+                    }
+                }
+                // 계정 출첵
+                if (checkBox3.Checked)
+                {
+                    if (DateTime.Now.Hour % 12 == 0 && DateTime.Now.Minute == 4 && DateTime.Now.Second == 0)
+                    {
+                        getACheck();
+                    }
+                }
+                
+                
+                //lg(string.Format(@"{0} {1} {2}",R,G,B)); // 특정 지점 RGB 값 확인
+            }
+            catch (System.FormatException w)
+            {
+                lg(w.Message);
+            }
+        }
+
+        // Pk시 텔
+        private async void playTel()
+        {
+            lg("PK 감지, 텔 동작");
+            await playTelTask();
+        }
+        private async Task playTelTask()
+        {
+            await Task.Delay(2000);
+            //InClick_y(907, 511);    // 귀환
+            InClick_y(844, 511);    // 텔
+            await Task.Delay(3000);
+        }
+
+        // 저 체력 귀환
+        private async void playGoHome()
+        {
+            lg("저 체력 귀환 시작");
+            await getACheckTask();
+            lg("저 체력 귀환 종료");
+        }
+        private async Task playGoHomeTask()
+        {
+            InClick_y(907, 511);    // 귀환
+            //InClick_y(844, 511);    // 텔
+            await Task.Delay(1500);
+        }
+
+        // 계정 출첵 받기
+        private async void getACheck()
+        {
+            lg("계정 출첵 받기 시작");
+            await getACheckTask();
+            lg("계정 출첵 받기 종료");
+        }
+        private async Task getACheckTask()
+        {
+            InClick_y(924, 69);     // 메뉴
+            await Task.Delay(1500);
+            InClick_y(807, 176);    // 출첵
+            await Task.Delay(3000);
+
+            InClick_y(924, 69);     // 메뉴
+            await Task.Delay(2000);
+            InClick_y(924, 69);     // 메뉴
+            await Task.Delay(1500);
+        }
+
+        // 혈맹 출첵 받기
+        private async void getGCheck()
+        {
+            lg("혈맹 출첵 받기 시작");
+            await getGCheckTask();
+            lg("혈맹 출첵 받기 종료");
+        }
+        private async Task getGCheckTask()
+        {
+            InClick_y(924, 69);     // 메뉴
+            await Task.Delay(1500);
+            InClick_y(697, 226);    // 혈맹
+            await Task.Delay(3000);
+
+            InClick_y(906, 160);    // 출첵
+            await Task.Delay(3000);
+
+            InClick_y(924, 69);     // 메뉴
+            await Task.Delay(2000);
+            InClick_y(924, 69);     // 메뉴
+            await Task.Delay(2000);
+            InClick_y(924, 69);     // 메뉴
+            await Task.Delay(1500);
+        }
+
+        // 우편물 받기
+        private async void getPost()
+        {
+            lg("우편물 받기 시작");
+            await getPostTask();
+            lg("우편물 받기 종료");
+        }
+        private async Task getPostTask()
+        {
+            InClick_y(924, 69);
+            await Task.Delay(1500);
+            InClick_y(753, 282);
+            await Task.Delay(1500);
+            InClick_y(840, 537);
+            await Task.Delay(1500);
+
+            InClick_y(924, 69);
+            await Task.Delay(1500);
+            InClick_y(924, 69);
+            await Task.Delay(1500);
+        }
+
+        //searchIMG에 스크린 이미지와 찾을 이미지를 넣어줄거에요
+        public bool searchIMG(Bitmap screen_img, Bitmap find_img)
+        {
+            //스크린 이미지 선언
+            using (Mat ScreenMat = OpenCvSharp.Extensions.BitmapConverter.ToMat(screen_img))
+            //찾을 이미지 선언
+            using (Mat FindMat = OpenCvSharp.Extensions.BitmapConverter.ToMat(find_img))
+            //스크린 이미지에서 FindMat 이미지를 찾아라
+            using (Mat res = ScreenMat.MatchTemplate(FindMat, TemplateMatchModes.CCoeffNormed))
+            {
+                //찾은 이미지의 유사도를 담을 더블형 최대 최소 값을 선언합니다.
+                double minval, maxval = 0;
+                //찾은 이미지의 위치를 담을 포인트형을 선업합니다.
+                OpenCvSharp.Point minloc, maxloc;
+                //찾은 이미지의 유사도 및 위치 값을 받습니다. 
+                Cv2.MinMaxLoc(res, out minval, out maxval, out minloc, out maxloc);
+                Debug.WriteLine("찾은 이미지의 유사도 : " + maxval);
+
+                if (maxval >= 0.8)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void searchIMG_Click(Bitmap screen_img, Bitmap find_img)
+        {
+            //스크린 이미지 선언
+            using (Mat ScreenMat = OpenCvSharp.Extensions.BitmapConverter.ToMat(screen_img))
+            //찾을 이미지 선언
+            using (Mat FindMat = OpenCvSharp.Extensions.BitmapConverter.ToMat(find_img))
+            //스크린 이미지에서 FindMat 이미지를 찾아라
+            using (Mat res = ScreenMat.MatchTemplate(FindMat, TemplateMatchModes.CCoeffNormed))
+            {
+                //찾은 이미지의 유사도를 담을 더블형 최대 최소 값을 선언합니다.
+                double minval, maxval = 0;
+                //찾은 이미지의 위치를 담을 포인트형을 선업합니다.
+                OpenCvSharp.Point minloc, maxloc;
+                //찾은 이미지의 유사도 및 위치 값을 받습니다. 
+                Cv2.MinMaxLoc(res, out minval, out maxval, out minloc, out maxloc);
+                Debug.WriteLine("찾은 이미지의 유사도 : " + maxval);
+
+                int offsetY = 35;
+                //이미지를 찾았을 경우 클릭이벤트를 발생!!
+                if (maxval >= 0.8)
+                {
+                    InClick(maxloc.X + FindMat.Width / 2, maxloc.Y + FindMat.Height / 2 - offsetY);
+                }
+            }
+        }
+
+        //x,y 값을 전달해주면 클릭이벤트를 발생합니다.
+        public void InClick(int x, int y)
+        {
+            //클릭이벤트를 발생시킬 플레이어를 찾습니다.
+            IntPtr findwindow = FindWindow(null, AppPlayerName);
+            if (findwindow != IntPtr.Zero)
+            {
+                //플레이어를 찾았을 경우 클릭이벤트를 발생시킬 핸들을 가져옵니다.
+                IntPtr hwnd_child = FindWindowEx(findwindow, IntPtr.Zero, "RenderWindow", "TheRender");
+                IntPtr lparam = new IntPtr(x | (y << 16));
+
+                //플레이어 핸들에 클릭 이벤트를 전달합니다.
+                SendMessage(hwnd_child, WM_LBUTTONDOWN, 1, lparam);
+                SendMessage(hwnd_child, WM_LBUTTONUP, 0, lparam);
+            }
+        }
+        public void InClick_y(int x, int y)
+        {
+            y -= 35;
+            //클릭이벤트를 발생시킬 플레이어를 찾습니다.
+            IntPtr findwindow = FindWindow(null, AppPlayerName);
+            if (findwindow != IntPtr.Zero)
+            {
+                //플레이어를 찾았을 경우 클릭이벤트를 발생시킬 핸들을 가져옵니다.
+                IntPtr hwnd_child = FindWindowEx(findwindow, IntPtr.Zero, "RenderWindow", "TheRender");
+                IntPtr lparam = new IntPtr(x | (y << 16));
+
+                //플레이어 핸들에 클릭 이벤트를 전달합니다.
+                SendMessage(hwnd_child, WM_LBUTTONDOWN, 1, lparam);
+                SendMessage(hwnd_child, WM_LBUTTONUP, 0, lparam);
+            }
+        }
+        private Bitmap PrintScreenAndCheck()
+        {
+            IntPtr findwindow = FindWindow(null, AppPlayerName);
+            if (findwindow != IntPtr.Zero)
+            {
+                //찾은 플레이어를 바탕으로 Graphics 정보를 가져옵니다.
+                Graphics Graphicsdata = Graphics.FromHwnd(findwindow);
+
+                //찾은 플레이어 창 크기 및 위치를 가져옵니다. 
+                Rectangle rect = Rectangle.Round(Graphicsdata.VisibleClipBounds);
+
+                //플레이어 창 크기 만큼의 비트맵을 선언해줍니다.
+                Bitmap bmp = new Bitmap(rect.Width, rect.Height);
+
+                //비트맵을 바탕으로 그래픽스 함수로 선언해줍니다.
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    //찾은 플레이어의 크기만큼 화면을 캡쳐합니다.
+                    IntPtr hdc = g.GetHdc();
+                    PrintWindow(findwindow, hdc, 0x2);
+                    g.ReleaseHdc(hdc);
+                }
+
+                // pictureBox1 이미지를 표시해줍니다.
+                pictureBox.Image = bmp;
+
+                // 특정 지점 RGB 컬러 저정
+                Color clr = bmp.GetPixel(97, 55);
+                R = clr.R;
+                G = clr.G;
+                B = clr.B;
+
+                return bmp;
+            }
+            else
+            {
+                //플레이어를 못찾을경우
+                lg("플레이어 찾을 수 없음");
+                return null;
+            }
+        }
+        private void lg(string str)
+        {
+            listBox.Items.Insert(0, DateTime.Now.ToString("HH:mm:ss")+" - "+str);
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            AppPlayerName = textBox.Text;
+            if(AppPlayerName.Length == 0)
+            {
+                MessageBox.Show("플레이어 이름 입력");
+                return;
+            }
+            lg("시작");
+            timer.Start();
+        }
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            lg("정지");
+            timer.Stop();
+        }
+    }
+}
